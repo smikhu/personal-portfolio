0f233f0ba03059e04d9a56f1929160178f36da84
Steven Mikhu <StevenMikhu@gmail.com>
2022-07-05 23:01:58 -0400

    icon set for portfolio tab

--- src/components/styles/MediaQueries.js
+++ src/components/styles/MediaQueries.js
@@ -10,26 +10,10 @@ const size = {
 };
 
 export const device = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  //   tablet: `(min-width: ${size.tablet})`,
-  //   laptop: `(min-width: ${size.laptop})`,
-
   potrait: `(min-width: ${size.tablet}) and (max-width: ${size.laptop})`,
   tablet: `(min-width: ${size.mobileXL}) and (max-width: ${size.tablet})`,
   tabletS: `(min-width: ${size.mobileL}) and (max-width: ${size.mobileXL})`,
   phone: `(min-width: ${size.mobileS}) and (max-width: ${size.mobileL})`,
-
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
 };
 
-// @media only screen
-//   and (min-device-width: 768px)
-//   and (max-device-width: 1024px)
-//   and (orientation: portrait)
-//   and (-webkit-min-device-pixel-ratio: 1) {
 
-// }
